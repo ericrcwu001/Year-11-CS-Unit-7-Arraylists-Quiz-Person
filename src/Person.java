24c9484e460baf91e24d03e0329c6d3ade3cb0a5
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2023-03-06 12:18:51 +0800

    finished

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,20 @@
 public class Person {
-
     private String firstName;
     private String lastName;
     private String age;
+
+    public Person(String firstName, String lastName, String age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getAge() {return age;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setAge(String age) {this.age = age;}
+    public String toString() {return "{firstName: " + firstName + ", lastName: " + lastName + ", age: " + age + "}";}
 
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -2,21 +2,44 @@ import java.util.ArrayList;
 
 public class PersonFactory {
 
-    private ArrayList<Person> allPersons;
+    public ArrayList<Person> allPersons;
 
     public PersonFactory() {
         allPersons = new ArrayList<>();
     }
 
     public void addPerson(Person person) {
+        if (allPersons.size() == 0) {
+            allPersons.add(person);
+            return;
+        }
 
+        int i = 0;
+        for (Person p : allPersons) {
+            if (person.getLastName().compareTo(p.getLastName()) < 0) {
+                allPersons.add(i, person);
+                break;
+            }
+            i++;
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under18 = new ArrayList<>();
+        for (Person p: allPersons) {
+            if (Integer.parseInt(p.getAge()) < 18) {
+                under18.add(p);
+            }
+        }
+        return under18;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for (Person p: allPersons) {
+            if (p.getLastName().equals(lastName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
